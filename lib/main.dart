4f7406e0892a0a967cfc65c428652d374ce673f7
Doam07 <davidomar41@gmail.com>
2023-10-08 10:49:05 -0600

    TAREA CALCULADORA 1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:calculadora/event_calc/calc_bloc.dart';
+import 'package:calculadora/screens/calculator_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [
+      BlocProvider<CalculatorBloc>(create: (_) => CalculatorBloc())
+    ], child: MyApp());
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: CalculatorScreen(),
+      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.indigo),
+    );
+  }
+}
